6cfbe06595424ec74f142f0dd73561a1c251e6ef
Hélder Pestana <hgpestana@gmail.com>
2019-04-20 16:20:50 +0100

    Finished setting up Webpack and React.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,2 @@
 // assets/js/app.js
-
-require('../css/app.css');
-
-console.log('Hello Webpack Encore');
+require('../css/app.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// directory where compiled assets will be stored
+    // directory where compiled assets will be stored
     .setOutputPath('web/build/')
+
     // public path used by the web server to access the output path
     .setPublicPath('/build')
+
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -18,26 +20,28 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
     .cleanupOutputBeforeBuild()
+
     .enableSourceMaps(!Encore.isProduction())
+
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
 
-// uncomment if you use Sass/SCSS files
-//.enableSassLoader()
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
 
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
+    // enable React
+    .enableReactPreset()
+
+    // uncomment if you're having problems with a jQuery plugin
 ;
 
 module.exports = Encore.getWebpackConfig();
